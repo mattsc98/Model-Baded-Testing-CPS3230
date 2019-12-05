6ac62c0e6dbf76b537d92fbc9d55cd8bd230c307
Matthew Schembri <mattsc98@gmail.com>
2019-12-05 13:06:25 +0100

    Initial Commit
    initial commit

---
+++ src/test/java/ScanMalta/ScanModel.java
@@ -0,0 +1,4 @@
+package ScanMalta;
+
+public class ScanModel {
+}
---
+++ src/test/java/ScanMalta/enums/ScanMaltaStates.java
@@ -0,0 +1,10 @@
+package ScanMalta.enums;
+
+public enum ScanMaltaStates {
+    LOGGED_IN,
+    LOGGED_OUT,
+    SEARCHING,
+    ADDING_TO_CART,
+    REMOVING_FROM_CART,
+    CHECKING_OUT
+}
